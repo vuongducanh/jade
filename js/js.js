47e9b1bde1e86e7c08eef1ef2c8363909e0f1f06
Thao Pham <thao.pham@ntq-solution.com.vn>
2018-07-26 16:54:18 +0700

    update jade

--- js/js.js
+++ js/js.js
@@ -4,3 +4,29 @@ $('.slider').slick({
   slidesToScroll: 1,
   dots: true,
 });
+
+
+$('.js-heading').click(function(e) {
+  e.preventDefault();
+
+  var $this = $(this);
+  var $subitems = $('.js-subitems');
+  var $heading = $('.js-heading');
+  var $item = $this.parent().find('.js-subitems');
+
+  var show = 'show';
+  var rotate = 'rotate-icon';
+
+
+  if ($item.hasClass(show)) {
+    $item.removeClass(show);
+    $this.removeClass(rotate);
+    $heading.removeClass(rotate);
+  } else {
+    $subitems.removeClass(show);
+    $item.addClass(show);
+    $heading.removeClass(rotate);
+    $this.addClass(rotate);
+  }
+});
+
